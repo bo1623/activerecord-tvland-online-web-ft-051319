814a5928a433baa05a5d74527f13da8af931047c
Boon Sim Oh <boonsim.oh15@gmail.com>
2019-06-17 13:30:14 +0800

    Done.

--- app/models/show.rb
+++ app/models/show.rb
@@ -1,3 +1,7 @@
 class Show < ActiveRecord::Base
-  
+
+  has_many :characters
+  has_many :actors, through: :characters
+  belongs_to :network #creates #build_network method which creates the belongs_to association over here
+
 end
